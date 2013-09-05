a11e302f2a920222a88418ec2ce7da0123d31a1d
Nidhi Gupta <nidhi.gupta@kayako.com>
2013-09-05 13:46:17 +0530

    Drupal module files {nb}

---
+++ kayako/resources/js/kayako.js
@@ -0,0 +1,29 @@
+/*
+var updateTicketPost = function () {
+    var thisCheck = jQuery(this);
+    */
+/*        if (thisCheck.is(':checked')) {
+     jQuery('#edit-kayako-ticket-new-post').removeAttr('disabled');
+     } else {
+     jQuery('#edit-kayako-ticket-new-post').attr('disabled', 'true');
+     }*//*
+
+
+    if (thisCheck.is(':checked')) {
+        jQuery('#edit-kayako-ticket-new-post').show();
+    } else {
+        jQuery('#edit-kayako-ticket-new-post').hide();
+    }
+}
+
+*/
+/*var addTicketFile = function (_namePrefix) {
+    jQuery('#' + _namePrefix + 'attachmentcontainer').append('<div class="ticketattachmentitem"><div class="ticketattachmentitemdelete" onclick="javascript: jQuery(this).parent().remove();">&nbsp;</div><input name="' + _namePrefix + 'attachments[]" type="file" size="20" class="swifttextlarge swifttextfile" /></div>');
+};*//*
+
+
+jQuery(document).ready(function () {
+    updateTicketPost();
+    jQuery('#edit-kayako-post-reply-checkbox').click(updateTicketPost);
+
+})*/
